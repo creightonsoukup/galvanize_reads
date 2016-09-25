62d62492e4e0779dfc79df233b804280c4f9d2fa
Your Name <creighton@creightonsoukup.com>
2016-09-24 22:12:31 -0600

    added edit feature

--- routes/books.js
+++ routes/books.js
@@ -19,6 +19,12 @@ router.get("/delete/:id", function (req,res,next) {
   })
 })
 
+router.get("/edit/:id", function (req,res,next) {
+  knex('book').select().where('id', req.params.id).then(function (book) {
+    res.render('edit_book', {book:book[0]})
+  })
+})
+
 router.post('/', function (req,res,next) {
   req.checkBody("title", "Title is empty or too long").notEmpty().isLength({max:255});
   req.checkBody("genre", "Genre is empty or too long").notEmpty().isLength({max: 255});
@@ -40,11 +46,34 @@ router.post('/', function (req,res,next) {
   }
 })
 
+router.put('/:id', function (req,res,next) {
+  req.checkBody("title", "Title is empty or too long").notEmpty().isLength({max:255});
+  req.checkBody("genre", "Genre is empty or too long").notEmpty().isLength({max: 255});
+  req.checkBody("description", "Description is missing or too long").notEmpty().isLength({max:2000})
+  req.checkBody("cover_image_url", "Not a URL").isURL(req.body.cover_url);
+
+  var errors = req.validationErrors();
+  if (errors) {
+    response.render("error", {errors:errors});
+  } else {
+    knex('book').update({
+      title: req.body.title,
+      genre: req.body.genre,
+      description: req.body.description,
+      cover_url: req.body.cover_url
+    }).then(function(){
+      res.redirect('/books')
+    });
+  }
+})
+
 router.delete('/:id', function (req,res,next) {
   console.log('got to delete');
   knex('book').del().where('id', req.params.id).then(function(){
     res.redirect('/books')
   })
 })
+
+
 
 module.exports = router;
