cb5bc6f62924223026662bcade013ced0e3e58c6
Danny Fritz <dannyfritz@gmail.com>
2016-09-26 10:24:59 -0600

    🚧 WIP

--- passport.js
+++ passport.js
@@ -2,8 +2,8 @@ var passport = require('passport')
 var googleOAuth = require('passport-google-oauth20').Strategy
 
 passport.use(new googleOAuth({
-  clientID: process.env.G_CLIENTID
-  clientSecret: process.env.G_CLIENTSECRET
+  clientID: process.env.G_CLIENTID,
+  clientSecret: process.env.G_CLIENTSECRET,
   callbackURL: process.env.G_CALLBACK
 },
   function (accessToken, refreshToken, profile, done) {
@@ -12,12 +12,14 @@ passport.use(new googleOAuth({
   }
 ))
 
-passport.serialize(function (user) {
+passport.serializeUser(function (user) {
   console.log("SERIALIZE")
   throw new Error("NOT INITIALIZED")
 })
 
-passport.deserialize(function (id) {
+passport.deserializeUser(function (id) {
   console.log("DESERIALIZE")
   throw new Error("NOT INITIALIZED")
 })
+
+module.exports = passport
--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 var passport = require('../passport')
 
-router.get('/google', passport.authenticate("google"))
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
+router.get('/google', passport.authenticate("google", { scope: ['profile'] }))
 
 module.exports = router
